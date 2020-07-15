160bb79063d8c6dfd64557a8f6e7ee309da9bdcf
Surajhomesfy <suraj.h@homesfy.in>
2020-07-15 12:28:58 +0530

    fonts push

--- application/views/homesfy/header.php
+++ application/views/homesfy/header.php
@@ -7,16 +7,27 @@
     <meta name="description" content="<?=$description;?>" data-react-helmet="true">
     <meta name="keywords" content="<?=$description;?>" data-react-helmet="true">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
-    <?php
-if (!empty($css)) {
+  	<link href=<?=$base_url."css/assests/fontawesome-free/css/all.min.css"?> rel="stylesheet" type="text/css">
 
-	foreach ($css as $value) {
-		echo '<link href="' . $value . '" rel="stylesheet" type="text/css">' . "\n";
-	}
-}
-?>
+    <?php
+		if (!empty($css)) {
+
+			foreach ($css as $value) {
+				echo '<link href="' . $value . '" rel="stylesheet" type="text/css">' . "\n";
+			}
+		}
+	?>
+	<?php
+		if (!empty($js)) {
+
+			foreach ($js as $value) {
+				echo '<script src="' . $value . '"></script>' . "\n";
+			}
+		}
+	?>
 </head>
 <nav class="navbar navbar-expand-lg navbar-light bgCommon sticky-top p-2 homeBorder">
 	<a class="navbar-brand" href="/">
@@ -28,10 +39,12 @@ if (!empty($css)) {
 		echo '<div class="searchCommon">
 		        <div class="searchCombo">
 		            <div class="d-flex justify-content-center">
-		                <div role="combobox" aria-haspopup="listbox" aria-owns="react-autowhatever-1" aria-expanded="false" class="react-autosuggest__container"><input type="text" autocomplete="off" aria-autocomplete="list" aria-controls="react-autowhatever-1" class="react-autosuggest__input" placeholder="Search By location, Builder Or Project" value="">
-		                    <div id="react-autowhatever-1" role="listbox" class="react-autosuggest__suggestions-container"></div>
+		                <div role="combobox" aria-haspopup="listbox" aria-owns="react-autowhatever-1" aria-expanded="false" class="react-autosuggest__container">
+		                <input type="text" id="name" autocomplete="off" aria-autocomplete="list" aria-controls="react-autowhatever-1" class="react-autosuggest__input" placeholder="Search By location, Builder Or Project" value="">
+		                    <div id="searchsuggestion" role="listbox" class="react-autosuggest__suggestions-container">
+		                    </div>
 		                </div>
-		                <div class="input-group-append"><button class="input-group btn text-white">Search</button></div>
+		                <div class="input-group-append"><button class="input-group search_button btn text-white">Search</button></div>
 		            </div>
 		        </div>
 		    </div>';
